1e703835f584d23f6c613d04d3749352d4314369
ahmedmooo1322 <ahmedelbahet00@gmail.com>
2026-07-04 17:17:51 +0300

    Delete tools/windows directory

--- tools/windows/04-build-release-apk.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-cd edutrack_app
-flutter build apk --release
-
-echo.
-echo Release APK output:
-echo edutrack_app\build\app\outputs\flutter-apk\app-release.apk
-echo.
-echo Note: For Play Store or public release you must configure proper app signing first.
